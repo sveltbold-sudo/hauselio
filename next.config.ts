55ad2cdd131435380c72f9e9b5089bfbf2d04778
sveltbold-sudo <sveltbold@gmail.com>
2026-08-22 17:53:41 +0200

    feat: disable Sentry source map uploads to speed up build

--- next.config.ts
+++ next.config.ts
@@ -81,4 +81,7 @@ export default withSentryConfig(nextConfig, {
   silent: true,
   org: process.env.SENTRY_ORG,
   project: process.env.SENTRY_PROJECT,
+  sourcemaps: {
+    disable: true,
+  },
 });
