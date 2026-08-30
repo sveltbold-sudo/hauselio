e227315a0b9f0b029a908215eaec8ad079d0ce58
sveltbold-sudo <sveltbold@gmail.com>
2026-08-30 09:46:00 +0200

    fix: remove invalid 'quality' key from images config, add 28 missing product brands (Trotec, Klarstein, Stadler Form, Comfee, etc.)

--- next.config.ts
+++ next.config.ts
@@ -48,7 +48,6 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   images: {
-    quality: 85,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     remotePatterns: [
